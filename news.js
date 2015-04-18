f267773efeb9da1a792eb0be1c2f11af90a252e7
Daniel Cohen <eprouver@yahoo.com>
2015-04-18 17:15:32 +0100

    up to yep nope

--- news.js
+++ news.js
@@ -1,23 +1,73 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+	Router.route('/', function () {
+		this.render('welcome');
+	});
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+	Router.route('/look', function () {
+		this.render('look');
+	});
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+	//Welcome Functions
+	(function () {
+		Template.welcome.events( {
+			'click #start': function (e, t) {
+				AntiModals.overlay('start', {
+					modal: true,
+					overlayClass: 'primary'
+				});
+			},
+			'click #enter': function (e, t) {
+				AntiModals.overlay('enter', {
+					modal: true,
+					overlayClass: 'primary'
+				});
+			}
+		});
+
+		Template.start.events( {
+			'click .close': function (e, t) {
+				AntiModals.dismissOverlay(t.firstNode);
+			}
+		});
+
+		Template.enter.events( {
+			'click .close': function (e, t) {
+				AntiModals.dismissOverlay(t.firstNode);
+			},
+			'click #enter-ok': function (e, t) {
+				AntiModals.dismissOverlay(t.firstNode);
+				document.location.href = '/look';
+			}
+		});
+	})();
+
+
+	//Look Functions
+	(function () {
+		function lookYep(e, t){
+			alert('yep');
+		};
+		
+		function lookNope(e,t){
+			alert('nope')
+		};
+		
+		Template.look.events( {
+			'click .yep': lookYep,
+			'click .nope': lookNope
+		});
+
+		Template.look.gestures( {
+			'swiperight .current.headline': lookYep,
+			'swipeleft .current.headline': lookNope
+		});
+	})();
+
+
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+	Meteor.startup(function () {
+		// code to run on server at startup
+	});
 }
